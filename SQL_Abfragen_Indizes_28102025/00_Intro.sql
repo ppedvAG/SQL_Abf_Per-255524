3dab28ce4b60b4ed5c8c88ac659f10b9800ee6a3
PhilippLibowicz <83985701+PhilippLibowicz@users.noreply.github.com>
2025-10-29 09:49:07 +0100

    Add files via upload

---
+++ SQL_Abfragen_Indizes_28102025/00_Intro.sql
@@ -0,0 +1,14 @@
+/*
+	Themen:
+	- Datenbank Design
+	- Dateigruppen
+	- Partitionen
+	- Kompression
+	- Indizes
+	- MAXDOP
+	- Abfragespeicher
+	- SQL Server Profiler
+	- JOIN Arten
+	- Abfragepläne
+
+*/
